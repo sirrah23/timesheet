ec53c044edddffa535e7ae28479925fd9787eb6a
sirrah23 <hseth93@gmail.com>
2015-11-08 21:51:56 -0500

    Added one timer, need to add more timers

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Timesheet', task:null});
+  res.render('index', { title: 'Timesheet'});
 });
 
 module.exports = router;
