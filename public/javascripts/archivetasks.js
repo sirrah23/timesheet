6e332bc46fd352ee81c0413bbda8329f3eeb27f0
sirrah23 <hseth93@gmail.com>
2015-11-25 00:01:47 -0500

    Table -> JSON done, now to post

--- public/javascripts/archivetasks.js
+++ public/javascripts/archivetasks.js
@@ -21,6 +21,20 @@ function stopAllWatches(){
         allStopButtons[i].click();
     }
 }
+
+/*
+ *    Functions used to obtain 
+ *    task name and time of the task from the
+ *    DOM tree
+ */   
+function getNameOfTask(columns){
+    return columns[0].getElementsByTagName('input')[0].value
+}
+
+function getTimeOfTask(columns){
+    return timeOfTask = columns[1].getElementsByTagName('span')[0].innerText
+}
+
 /*
 *   Takes the table of task, time pairs and turns them into 
 *   A JSON object so that it can be sent to the server for 
@@ -35,26 +49,30 @@ function tableToJson(){
     var tableJson = [];
     var columnsOfCurrRow, nameOfTask, timeOfTask;
     //Start at one to ignore column headers
-    for (var i = 1; i < rows; i++){
+    for (var i = 1; i < rows.length; i++){
         //For each row grab the task name and time and put into JSON object
         columnsOfCurrRow = rows[i].children
         nameOfTask = getNameOfTask(columnsOfCurrRow); 
         timeOfTask = getTimeOfTask(columnsOfCurrRow); 
-        tableJson.push({"task":nameOfTask, "time":timeOfTask};
+        tableJson.push({"task":nameOfTask, "time":timeOfTask});
     }
     return tableJson;
 }
 
-
 /*
- *    Functions used to obtain 
- *    task name and time of the task from the
- *    DOM tree
- */   
-function getNameOfTask(columns){
-    return columns[0].firstChild.nextSibling.value;
+*   This function takes the JSON table
+*   and posts it to /archive
+*/
+function postTableToServer(){
+    var form = {};
+    form.method = 'post';
+    form.action = '/archive';
+    var tableJson = tableToJson();
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open(form.method,form.action,true);
+    xhr.setRequestHeader('Content-Type','application/json; charset=UTF-8');
+
+    xhr.send(JSON.stringify(tableJson));
 }
 
-function getTimeOfTask(columns){
-    return timeOfTask = columns[1].firstChild.nextSibling.nextSibling.innerText
-}
